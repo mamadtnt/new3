6f295fab1dadfe77f62b44a8a5c697dd0bdc61e4
patrikrm13 <mahdi028a@gmail.com>
2016-02-15 22:42:11 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -6,7 +6,7 @@ local bot_id = our_id -- your bot id
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'گروه من نیست زحمت کشیدی که بلاکت کنم ^_^', ok_cb, false)
+      send_large_msg("chat#id"..msg.to.id, 'group mn nist sik report ^_^', ok_cb, false)
       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
       block_user("user#id"..msg.from.id,ok_cb,false)
     end
@@ -14,7 +14,7 @@ end
  
 return {
   patterns = {
-    "^[!/](sik)$",
+    "^[!/](leave)$",
     "^!!tgservice (.+)$",
   },
   run = run
